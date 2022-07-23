f6cd99ed7f59e9003fb370bccd3d6802f1b29c9c
RIKUGHI <jualonlinee3@gmail.com>
2022-07-23 16:33:24 +0700

    Membuat UserController

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    public function login()
+    {
+        
+    }
+
+    public function doLogin()
+    {
+        
+    }
+
+    public function doLogout()
+    {
+        
+    }
+}
