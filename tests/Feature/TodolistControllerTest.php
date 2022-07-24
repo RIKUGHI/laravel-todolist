a65624e3aa5452083b49f1d22ea6fd58ccaba2a2
RIKUGHI <jualonlinee3@gmail.com>
2022-07-24 10:49:19 +0700

    Membuat Add Todo Action

--- tests/Feature/TodolistControllerTest.php
+++ tests/Feature/TodolistControllerTest.php
@@ -28,4 +28,21 @@ class TodolistControllerTest extends TestCase
             ->assertSeeText('2')
             ->assertSeeText('Es');
     }
+
+    public function testAddTodoFailed()
+    {
+        $this->withSession([
+            'user' => 'bambang'
+        ])->post('/todolist', [])
+            ->assertSeeText('Todo is required');
+    }
+
+    public function testAddTodoSuccess()
+    {
+        $this->withSession([
+            'user' => 'bambang'
+        ])->post('/todolist', [
+            'todo' => 'Nice'
+        ])->assertRedirect('/todolist');
+    }
 }
