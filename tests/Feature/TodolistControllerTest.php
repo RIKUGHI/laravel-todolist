23cf240482129f946e567e0cc537b68e9bae66ee
RIKUGHI <jualonlinee3@gmail.com>
2022-07-24 10:36:26 +0700

    Membuat Todolist Page

---
+++ tests/Feature/TodolistControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodolistControllerTest extends TestCase
+{
+    public function testTodolist()
+    {
+        $this->withSession([
+            'user' => 'Bambang',
+            'todolist' => [
+                [
+                    'id' => '1',
+                    'todo' => 'Nice'
+                ],
+                [
+                    'id' => '2',
+                    'todo' => 'Es'
+                ]
+            ]
+        ])->get('/todolist')
+            ->assertSeeText('1')
+            ->assertSeeText('Nice')
+            ->assertSeeText('2')
+            ->assertSeeText('Es');
+    }
+}
